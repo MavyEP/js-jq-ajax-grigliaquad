829937ae5427da63844efa515b45dd033f4660b7
Edoardo Piergentili <epiergentili@outlook.it>
2020-05-19 19:00:41 +0200

    piccolo abbellimento

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
 
   var box = $(".box");
-
   var source   = $("#template").html();
   var template = Handlebars.compile(source);
 
   var number_in_box = {
    number_box : ''
   };
-
 
     box.click(function (){
       var selected_box = $(this);
@@ -17,18 +15,14 @@ $(document).ready(function(){
             'url' : "https://flynn.boolean.careers/exercises/api/random/int",
             'method' : "GET" ,
             'success' : function (data) {
-              var number = data.response;
-              number_in_box.number_box = number;
-              var html = template(number_in_box);
-              selected_box.append(html);
-              if (number <= 5) {
-                selected_box.addClass("yellow used");
-              } else {
-                selected_box.addClass("green used");
-              }
+                  var number = data.response;
+                  number_in_box.number_box = number;
+                  var html = template(number_in_box);
+                  selected_box.append(html);
+                  changecolor(number ,selected_box);
             },
             'error' : function () {
-              alert("NON FUNZIONA LA RICHIESTA ajax!");
+                  alert("NON FUNZIONA LA RICHIESTA ajax!");
             }
         });
       };
@@ -36,6 +30,15 @@ $(document).ready(function(){
 
 });
 
+
+
+function changecolor(number , selector) {
+  if (number <= 5) {
+    selector.addClass("yellow used");
+  } else {
+    selector.addClass("green used");
+  }
+}
 
 // COME FARE PER RITARDARE DEGLI EVENTI AD ESEMPIO CONTROLLARE SE QUANTI DIV HANNO
 // LA CLASSE USED SENZA AVERE IL PROBLEMA DI CHI VIENE PRIMA.
